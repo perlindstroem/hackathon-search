20e05586146d4726663e4b652df2a06ecc3de8d7
oscarpap <oscpa453@student.liu.se>
2017-11-03 11:37:21 +0100

    Added some cities and paths between

--- src/Runner.java
+++ src/Runner.java
@@ -27,20 +27,73 @@ public class Runner {
 
         World worldOne = new World();
 
-        City linkan = new City("linkan");
-        City norpan = new City("norpan");
+        City sodertalje = new City("sodertalje");
+        City boras = new City("boras");
+        City gavle = new City("gavle");
+        City umea = new City("umea");
+        City lund = new City("lund");
+        City jonkoping = new City("jonkoping");
+        City helsingborg = new City("helsingborg");
+        City orebro = new City("orebro");
+        City vasteras = new City("vasteras");
+        City sollentuna = new City("sollentuna");
+        City uppsala = new City("uppsala");
+        City malmo = new City("malmo");
+        City goteborg = new City("goteborg");
+        City stockholm = new City("stockholm");
+
+        City linkoping = new City("linkoping");
+        City norrkoping = new City("norrkoping");
+       /*
         City malmslatt = new City("malmslatt");
         City ryd = new City("ryd");
         City skarblacka = new City("skarblacka");
         City soderkoping = new City("soderkoping");
 
-        worldOne.addCity(linkan);
-        worldOne.addCity(norpan);
         worldOne.addCity(malmslatt);
         worldOne.addCity(ryd);
         worldOne.addCity(skarblacka);
         worldOne.addCity(soderkoping);
+        */
 
+        worldOne.addCity(linkoping);
+        worldOne.addCity(norrkoping);
+        worldOne.addCity(sodertalje);
+        worldOne.addCity(boras);
+        worldOne.addCity(gavle);
+        worldOne.addCity(umea);
+        worldOne.addCity(lund);
+        worldOne.addCity(jonkoping);
+        worldOne.addCity(helsingborg);
+        worldOne.addCity(orebro);
+        worldOne.addCity(vasteras);
+        worldOne.addCity(sollentuna);
+        worldOne.addCity(uppsala);
+        worldOne.addCity(malmo);
+        worldOne.addCity(goteborg);
+        worldOne.addCity(stockholm);
+
+        worldOne.addNeighbours(malmo, lund, 17.8, BCT);
+        worldOne.addNeighbours(malmo, helsingborg, 54.2, CT);
+        worldOne.addNeighbours(lund, helsingborg, 49.5, T);
+        worldOne.addNeighbours(helsingborg, goteborg, 191, BT);
+        worldOne.addNeighbours(goteborg, boras, 58.8, BCT);
+        worldOne.addNeighbours(boras, jonkoping, 72.5, BCT);
+        worldOne.addNeighbours(lund, jonkoping, 238, C);
+        worldOne.addNeighbours(jonkoping, linkoping, 110, CT);
+        worldOne.addNeighbours(linkoping, norrkoping, 38.8, BT);
+        worldOne.addNeighbours(norrkoping, orebro, 93.6, CT);
+        worldOne.addNeighbours(norrkoping, sodertalje, 106, T);
+        worldOne.addNeighbours(orebro, vasteras, 84.1, CT);
+        worldOne.addNeighbours(sodertalje, stockholm, 29.6, T);
+        worldOne.addNeighbours(stockholm, sollentuna, 14.8, B);
+        worldOne.addNeighbours(sollentuna, uppsala, 50.6, BCT);
+        worldOne.addNeighbours(uppsala, gavle, 96.7, T);
+        worldOne.addNeighbours(vasteras, gavle, 122, C);
+        worldOne.addNeighbours(gavle, umea, 427, T);
+        worldOne.addNeighbours(orebro, sodertalje, 138, T);
+
+        /*
         worldOne.addNeighbours(linkan, norpan, 10.0, BCT);
         worldOne.addNeighbours(linkan, ryd, 4.0, BC);
         worldOne.addNeighbours(malmslatt, linkan, 6.0, BC);
@@ -48,10 +101,11 @@ public class Runner {
         worldOne.addNeighbours(norpan, skarblacka, 6.0, BC);
         worldOne.addNeighbours(soderkoping, norpan, 4.0, BCT);
         worldOne.addNeighbours(soderkoping, skarblacka, 2.0, C);
+        */
 
         worldOne.listCities();
 
-        List<City> tempList = worldOne.getNeighboursTo(linkan);
+        List<City> tempList = worldOne.getNeighboursTo(linkoping);
 
         for (City city : tempList){
             System.out.println("neighbour " + city.getName());
@@ -71,7 +125,7 @@ public class Runner {
         }
     }
 
-    public static void populateTransportLists(){
+    private static void populateTransportLists(){
 
         BCT.add(new Bike());
         BCT.add(new Car());
